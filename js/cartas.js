9e2cd9fbd4a94244cc643ba1bc8a3fc876173519
dubanyesid <dubanyesidsb@ufps.edu.co>
2021-04-16 07:45:46 -0500

    Falta seguir

---
+++ js/cartas.js
@@ -0,0 +1,5 @@
+"cartas": [{"numero": "1","carta": "As de Diamantes", "cant":"0"},
+	{"numero": "2","carta": "Dos de Diamantes", "cant":"0"},
+	{"numero": "3","carta": "Tres de Diamantes", "cant":"0"},
+	{"numero": "4","carta": "Cuatro de Corazones", "cant":"0"},
+	{"numero": "5","carta": "Cinco de Corazones", "cant":"0"}]
