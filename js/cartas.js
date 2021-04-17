1fd53733356bce4137b21edf8690851db4efb4a3
dubanyesid <dubanyesidsb@ufps.edu.co>
2021-04-16 19:03:56 -0500

    Js Terminado

--- js/cartas.js
+++ js/cartas.js
@@ -1,5 +1,29 @@
-"cartas": [{"numero": "1","carta": "As de Diamantes", "cant":"0"},
-	{"numero": "2","carta": "Dos de Diamantes", "cant":"0"},
-	{"numero": "3","carta": "Tres de Diamantes", "cant":"0"},
-	{"numero": "4","carta": "Cuatro de Corazones", "cant":"0"},
-	{"numero": "5","carta": "Cinco de Corazones", "cant":"0"}]
+function cargarTabla (){
+
+	const tabla = document.getElementById("tabla_carta");
+ 	const tr = document.createElement('tr');
+
+     for (i=0; i<input.length; i++) {
+
+        const tdNumero= document.createElement('td');
+            tdNumero.textContent = input[i].addEventListener;
+            tr.appendChild(tdNumero);
+
+            const tdCarta = document.createElement('td');
+            tdCarta.textContent =input[i].addEventListener;
+            tr.appendChild(tdCarta);
+            
+            tabla.appendChild(tr);
+    }
+    
+}
+
+
+function obtenerdatos(){
+    var url = "http://demo6497253.mockable.io/cartas";
+
+	fetch  (url)
+	    .then(response => response.json())
+		.then(data => generarTabla(data))
+	.catch(error => {});
+}
